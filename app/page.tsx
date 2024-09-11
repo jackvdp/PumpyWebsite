50fd164713df8dc95570f26eaf1fc83e897685f3
jackvdp <jack@pumpymusic.co.uk>
2024-09-11 17:11:50 +0100

    Remove unused declarations

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import AOS from 'aos';
