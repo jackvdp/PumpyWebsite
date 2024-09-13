67d2803a185ec8b92a0ee566dcf8ae252836f708
jackvdp <jack@pumpymusic.co.uk>
2024-09-13 15:42:04 +0100

    Split comapnies into different file

---
+++ components/reusables/Logos/companies.ts
@@ -0,0 +1,37 @@
+interface Company {
+    name: string;
+    imageUrl: string;
+    animate: boolean;
+}
+
+const companies: Company[] = [
+    {
+        name: 'Met Office',
+        imageUrl:
+            'https://www.metoffice.gov.uk/webfiles/1725362600156/images/logos/mo-green-white.svg',
+        animate: false,
+    },
+    {
+        name: 'Department for Business & Trade',
+        imageUrl:
+            'https://ukskillspartnership.org.uk/moogryce/2023/03/DBT_Red-transparent-background.png',
+        animate: true,
+    },
+    {
+        name: 'Gigabyte Software',
+        imageUrl: 'https://www.gigabyte.software/assets/images/logo/logo-dark.svg',
+        animate: true,
+    },
+    {
+        name: 'Elev8 Exchange',
+        imageUrl: 'https://www.elev8exchange.com/assets/images/logo.webp',
+        animate: true,
+    },
+    {
+        name: 'Vanderpump FX',
+        imageUrl: 'https://www.vanderpumpfx.com/img/logo%20medium.png',
+        animate: false,
+    },
+];
+
+export default companies;
