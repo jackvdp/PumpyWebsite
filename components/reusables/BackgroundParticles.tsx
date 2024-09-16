6c72f361a07274e0d50a8a86c7d8d334d67c1a1a
jackvdp <jack@pumpymusic.co.uk>
2024-09-16 17:08:33 +0100

    Fix background particles

--- components/reusables/BackgroundParticles.tsx
+++ components/reusables/BackgroundParticles.tsx
@@ -3,71 +3,75 @@ import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
 import { Container, ISourceOptions } from "@tsparticles/engine";
 
-export default function BackgroundParticles() {
-    const [init, setInit] = useState(false);
+interface BackgroundParticlesProps {
+  className?: string;
+  id: string;
+}
 
-    useEffect(() => {
-        initParticlesEngine(async (engine) => {
-            await loadSlim(engine);
-        }).then(() => {
-            setInit(true);
-        });
-    }, []);
+export default function BackgroundParticles({ className = "", id }: BackgroundParticlesProps) {
+  const [init, setInit] = useState(false);
 
-    const particlesLoaded = async (container?: Container): Promise<void> => {
-        console.log(container);
-    };
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
 
-    const particlesOptions: ISourceOptions = {
-        fpsLimit: 120,
-        particles: {
-            color: {
-                value: ["#a5b4fc", "#d8b4fe", "#f9a8d4", "#93c5fd", "#c4b5fd"],
-            },
-            move: {
-                direction: "none",
-                enable: true,
-                outModes: {
-                    default: "bounce",
-                },
-                random: false,
-                speed: 2,
-                straight: false,
-            },
-            number: {
-                density: {
-                    enable: true,
-                },
-                value: 5,
-            },
-            opacity: {
-                value: 0.4,
-            },
-            shape: {
-                type: "circle",
-            },
-            size: {
-                value: { min: 50, max: 150 },
-            },
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const particlesOptions: ISourceOptions = {
+    fpsLimit: 120,
+    particles: {
+      color: {
+        value: ["#a5b4fc", "#d8b4fe", "#f9a8d4", "#93c5fd", "#c4b5fd"],
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: {
+          default: "bounce",
         },
-        detectRetina: true,
-        fullScreen: {
-            enable: false,
-            zIndex: -1,
+        random: false,
+        speed: 2,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
         },
-    };
+        value: 5,
+      },
+      opacity: {
+        value: 0.4,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 50, max: 150 },
+      },
+    },
+    detectRetina: true,
+    fullScreen: {
+      enable: false,
+      zIndex: -1,
+    },
+  };
 
-    return (
-        <div className="absolute inset-0">
-            {init && (
-                <Particles
-                    id="tsparticles"
-                    particlesLoaded={particlesLoaded}
-                    options={particlesOptions}
-                    className="absolute inset-0"
-                />
-            )}
-            <div className="absolute inset-0 backdrop-filter backdrop-blur-md"></div>
-        </div>
-    );
+  return (
+    <div className={`absolute inset-0 ${className}`}>
+      {init && (
+        <Particles
+          id={id}
+          particlesLoaded={particlesLoaded}
+          options={particlesOptions}
+          className="absolute inset-0"
+        />
+      )}
+    </div>
+  );
 }
