ae45de19ac261c915a41a3cd07646326d157c260
jackvdp <jack@pumpymusic.co.uk>
2024-09-23 16:14:36 +0100

    Add my own domain to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['www.metoffice.gov.uk', 'ukskillspartnership.org.uk', 'www.gigabyte.software', 'www.elev8exchange.com', "as1.ftcdn.net", "as2.ftcdn.net", "source.unsplash.com", "www.vanderpumpfx.com", "www.electoralnetwork.org"],
+        domains: ['www.metoffice.gov.uk', 'ukskillspartnership.org.uk', 'www.gigabyte.software', 'www.elev8exchange.com', "as1.ftcdn.net", "as2.ftcdn.net", "source.unsplash.com", "www.vanderpumpfx.com", "www.electoralnetwork.org", "pumpy-website-vnb5.vercel.app"],
     },
 };
 
